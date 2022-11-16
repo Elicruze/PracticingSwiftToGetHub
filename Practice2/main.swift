c0bd5d9ffb273d98bcff166c6f9323e1cd2709ae
elicruze <elicruze@icloud.com>
2022-11-15 23:30:17 -0600

    Making something with everything I've learned for practice... might be a while.

--- Practice2/main.swift
+++ Practice2/main.swift
@@ -2,18 +2,53 @@
 //  main.swift
 //  Practice2
 //
-//  Created by Sabrina Elizondo on 11/14/22.
+//  Created by Elicruze on 11/14/22.
 //
 
 import Foundation
 
-//This is just to practice making major changes to the repo
+//This is just to practice making major changes to the repo...
 
-class ServiceAnimals {
-    var animalList: (String, Int) = ("Peacock", 0)
+/*Oddly I start creating an animal pet store kinda deal
+ don't know where I'm going with this but we will figure this out...*/
+
+class Animals {
+    var adminName:String?
+    let aniLimit:Int = 10
+    var animalAmount:Int = 0
+    var price:Double = 0.00
+    var indexChara:Character = "m"
+    var humanPetList = ["JaneDoe":0]
+    var animelNameList = ["Dog",
+                          "Cat",
+                          "Fish",
+                          "Lizard",
+                          "Bird"]
     
-    func addAnimal () {
+    func petListIndex () {
+        //Idk what your doing, think about it. 🤔
         
+    }
+    
+    
+    func addAni () {
+        var answer:String?
+        print ("You would like to add an animal?")
+        answer = readLine()!.lowercased()
+        if answer == "yes" || animalAmount > aniLimit
+        {animalAmount += 1; print("\nAnimal added, you have \(animalAmount)")
+            print ("What breed is the animal?")
+            for i in animelNameList {
+                print(i)
+            }
+            answer = nil; answer = readLine()!.lowercased()
+            //add more...
+        }
+        //If not do this...
+        else if answer == "no" {print("Animal not added, you have \(animalAmount)")}
+        else {print("Didn't do anything.")}
     }
 }
 
+let getAnimal = Animals()
+getAnimal.addAni()
